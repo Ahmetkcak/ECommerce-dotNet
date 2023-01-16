106469938122fa6ada65717b62aa9642366b2b2f
Ahmet Koçak <ahmet.aliberk07@gmail.com>
2023-01-16 16:49:07 +0300

    Add project files.

---
+++ ECommerce/Infrastructure/ECommerce.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,24 @@
+﻿using ECommerce.Application.Abstract;
+using ECommerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> getAll()
+            =>new () 
+            {
+                new(){Id= 1,Name="Bardak",Price=100,Stock=12},
+                new(){Id= 2,Name="Bardak 1",Price=200,Stock=100},
+                new(){Id= 3,Name="Bardak 2",Price=220,Stock=9},
+                new(){Id= 4,Name="Bardak 3",Price=155,Stock=15},
+                new(){Id= 5,Name="Bardak 4",Price=3600,Stock=10},
+
+            };
+    }
+}
---
+++ ECommerce/Presentation/ECommerce.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using ECommerce.Application.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private  IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public IActionResult getAll()
+        {
+            var products = _productService.getAll();
+            return Ok(products);
+        }
+    }
+}
