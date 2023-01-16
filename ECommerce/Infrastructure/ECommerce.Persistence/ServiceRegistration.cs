3ebf64fa6a0af8849f064601d331eac7f91b0ed4
Ahmet Koçak <ahmet.aliberk07@gmail.com>
2023-01-17 00:34:31 +0300

    add Order, Customer entities,migration and connection string

--- ECommerce/Infrastructure/ECommerce.Persistence/ServiceRegistration.cs
+++ ECommerce/Infrastructure/ECommerce.Persistence/ServiceRegistration.cs
@@ -1,11 +1,14 @@
-﻿using ECommerce.Application.Abstract;
+﻿using Microsoft.EntityFrameworkCore;
+using ECommerce.Application.Abstract;
 using ECommerce.Persistence.Concretes;
+using ECommerce.Persistence.Context;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ECommerce.Persistence.Configurations;
 
 namespace ECommerce.Persistence
 {
@@ -14,6 +17,7 @@ namespace ECommerce.Persistence
         public static void AddPersistenceServices(this IServiceCollection services)
         {
             services.AddSingleton<IProductService, ProductService>();
+            services.AddDbContext<ECommerceDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
         }
     }
 }
