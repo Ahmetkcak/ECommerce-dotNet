6e5484892bec2b24784388e54264c32c275f9c73
Ahmetkcak <ahmet.aliberk07@gmail.com>
2023-03-09 22:43:30 +0300

    add BasketsController

---
+++ ECommerce/Core/ECommerce.Application/Features/Commands/Basket/RemoveBasketItem/RemoveBasketItemCommandRequest.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace ECommerce.Application.Features.Commands.Basket.RemoveBasketItem
+{
+    public class RemoveBasketItemCommandRequest : IRequest<RemoveBasketItemCommandResponse>
+    {
+        public int BasketItemId { get; set; }
+    }
+}
---
+++ ECommerce/Core/ECommerce.Application/Features/Queries/Basket/GetBasketItems/GetBasketItemsQueryResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerce.Application.Features.Queries.Basket.GetBasketItems
+{
+    public class GetBasketItemsQueryResponse
+    {
+        public int BasketItemId { get; set; }
+        public string ProductName { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
